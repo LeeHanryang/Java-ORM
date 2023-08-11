b15adbc672f851280f3e0098928a9f195bfca337
이준혁 <ballack02@naver.com>
2023-08-11 11:18:03 +0900

    중간 저장 EntityManagerFactory EntityManager 동작 방식 EntityTransaction 내에서만 실행됨

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,18 @@
+package hellojpa;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+    @Id
+    private Long id;
+    private String name;
+
+}
