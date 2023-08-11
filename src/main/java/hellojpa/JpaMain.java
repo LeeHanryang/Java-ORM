d6d51d0345b8891cb150ea7819c50c95ae2787c5
이준혁 <ballack02@naver.com>
2023-08-11 15:41:23 +0900

    중간 저장 DB 조회 후 영속성 컨텍스트 내부 저장 -> 캐시 조회

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,26 +17,9 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
             // 영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");
-
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember.getId = " + findMember.getId());
-            System.out.println("findMember.getId = " + findMember.getName());
-
-            // 준영속
-//            em.detach(member);
-
-            // 삭제
-//            em.remove(member);
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
 
             tx.commit();
 
