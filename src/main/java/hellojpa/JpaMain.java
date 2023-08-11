8f86cf8ad0501e44158dd39f3c8814106d203e55
이준혁 <ballack02@naver.com>
2023-08-11 17:06:06 +0900

    중간 저장 DB 조회 후 영속성 컨텍스트 내부 저장 -> 캐시 조회 비교

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,6 +21,8 @@ public class JpaMain {
             Member findMember1 = em.find(Member.class, 101L);
             Member findMember2 = em.find(Member.class, 101L);
 
+            System.out.println("(findMember1 == findMember2) = " + (findMember1 == findMember2));
+            
             tx.commit();
 
         } catch (Exception e) {
