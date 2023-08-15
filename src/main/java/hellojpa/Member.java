76a1992e962ff8803006e87d8d33e176a6b743a3
이준혁 <ballack02@naver.com>
2023-08-16 08:54:40 +0900

    중간 저장 @Enumerated(EnumType.STRING) - ORDINAL 옵션 사용 자제
    LocalDate, LocalDateTime은 각각 DB에서 Date, DateTime으로 저장됨

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
@@ -12,12 +14,12 @@ public class Member {
     @Id
     private Long id;
 
-    @Column(name = "name", columnDefinition = "varchar(100) default 'EMPTY'")
+    @Column(name = "name", nullable = false)
     private String username;
 
     private Integer age;
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING) // 열거형 - ORDINAL 옵션 사용 시 순서 문제 있음, 무조건 STRING으로
     private RoleType roleType;
 
     @Temporal(TemporalType.TIMESTAMP)
@@ -25,6 +27,9 @@ public class Member {
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastModifiedDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
 
     @Lob
     private String description;
