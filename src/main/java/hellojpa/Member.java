edba2797856cd4b42de8e2f2e51f863d31be4a00
이준혁 <ballack02@naver.com>
2023-08-16 10:07:29 +0900

    중간 저장 @GeneratedValue 옵션

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,38 +4,28 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ",
+        allocationSize = 1
+)
 @Getter @Setter
 public class Member {
-    @Id
+    @Id // 기본 키 매핑
+    @GeneratedValue(
+//            strategy = GenerationType.AUTO // 아래 세가지 옵션 중 자동
+//            strategy = GenerationType.IDENTITY // - 기본키 생성을 DB에 위임
+//            strategy = GenerationType.SEQUENCE // - 시퀀스를 주는 전략
+            strategy = GenerationType.TABLE // - 시퀀스 테이블 생성
+            , generator = "MEMBER_SEQ_GENERATOR"
+    )
     private Long id;
 
     @Column(name = "name", nullable = false)
     private String username;
-
-    private Integer age;
-
-    @Enumerated(EnumType.STRING) // 열거형 - ORDINAL 옵션 사용 시 순서 문제 있음, 무조건 STRING으로
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    private LocalDate testLocalDate;
-    private LocalDateTime testLocalDateTime;
-
-    @Lob
-    private String description;
-
-    @Transient
-    private int temp;
 
     protected Member() {}
 
