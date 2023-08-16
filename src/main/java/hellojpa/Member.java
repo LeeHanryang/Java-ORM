f899010f8ceaa985f51fe7a8c26c2c015b134d6e
이준혁 <ballack02@naver.com>
2023-08-16 10:15:19 +0900

    중간 저장 IDENTITY 에선 DB에 INSERT 되는 시점에만 KEY를 알고 있기 때문에 persist 시 바로 flush를 하여 KEY를 영속성 컨텍스트에 저장한다

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -15,13 +15,7 @@ import javax.persistence.*;
 @Getter @Setter
 public class Member {
     @Id // 기본 키 매핑
-    @GeneratedValue(
-//            strategy = GenerationType.AUTO // 아래 세가지 옵션 중 자동
-//            strategy = GenerationType.IDENTITY // - 기본키 생성을 DB에 위임
-//            strategy = GenerationType.SEQUENCE // - 시퀀스를 주는 전략
-            strategy = GenerationType.TABLE // - 시퀀스 테이블 생성
-            , generator = "MEMBER_SEQ_GENERATOR"
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name", nullable = false)
